31349a700ac2dd444891498d270a2d5d5cbd9cf3
ArturButs <butsarthur@gmail.com>
2025-03-05 13:50:21 +0300

    put code into functions

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
-			lastWatchedMoviesA = prompt("Один из последних просмотренных фильмов?", ""),
-			lastWatchedMoviesRateA = +prompt("На сколько оцените его?", ""),
-			lastWatchedMoviesB = prompt("Один из последних просмотренных фильмов?", ""),
-			lastWatchedMoviesRateB = +prompt("На сколько оцените его?", "");
+"use strict"
+
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+	}
+}
+
+start();
 
 
 const personalMovieDB = {
@@ -13,7 +21,38 @@ const personalMovieDB = {
 	privat: false,
 }
 
-personalMovieDB.movies[lastWatchedMoviesA] = lastWatchedMoviesRateA;
-personalMovieDB.movies[lastWatchedMoviesB] = lastWatchedMoviesRateB;
+
+function rememberMyFilms() {
+
+	for (let i = 0; i < 2; i++) {
+		const lastWatchedMovies = prompt("Один из последних просмотренных фильмов?", "");
+		const lastWatchedMoviesRate = +prompt("На сколько оцените его?", "");
+	
+		if (lastWatchedMovies != null && lastWatchedMoviesRate != null && lastWatchedMovies != '' && lastWatchedMoviesRate != '' && lastWatchedMovies.length < 50) {
+			personalMovieDB.movies[lastWatchedMovies] = lastWatchedMoviesRate;
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+
+}
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотрено довольно мало фильмов')
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель');
+	} else if (personalMovieDB.count >= 30){
+		console.log('Вы киноман');
+	} else {
+		console.log('error');
+	}
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDB)
